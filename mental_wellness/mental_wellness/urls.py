3b2d645e1a14c793436bed4127a2cdc1b54c44d9
GodaniShubham <godanishubham30@gmail.com>
2025-08-26 00:23:52 +0530

    new env changes

--- mental_wellness/mental_wellness/urls.py
+++ mental_wellness/mental_wellness/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from saharathiapp import views   # <-- apne app ka views import karo
 
 urlpatterns = [
     path('admin/', admin.site.urls),

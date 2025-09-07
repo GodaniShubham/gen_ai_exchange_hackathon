fde19be5e0c84dda820e7f22974a233be697da16
Karthik Appalla <appallakarthik07@gmail.com>
2025-09-07 23:33:54 +0530

    commits

--- mental_wellness/saharathiapp/urls.py
+++ mental_wellness/saharathiapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
-path("login/", views.login_view, name="login"),
-path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
     # Chat Page (frontend UI)
     path("chat/", views.chat_page, name="chat_page"),
     path("analysis/", views.analysis, name="analysis"),
